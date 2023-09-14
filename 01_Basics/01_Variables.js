4eba8e3b8ccff2fa3ab8e540482340263b0ffabd
Byte Developer <145024847+bytesSculptor@users.noreply.github.com>
2023-09-14 14:51:08 +0000

    dataTypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 52365;
+let accountEmail = "bytesSculptor@github.com";
+var accountPassword = 90123;
+accountCity = "Mumbai";
+let accountState;
+
+
+// accountId = 123 // cannot change const 
+// console.log(accountEmail);
+accountEmail = "newemail@gmail.com";
+accountPassword = "newPasscode";
+accountCity = "Delhi";
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
