5bc08a98e40c6b557348f13e6a534cb8e9fa77ed
Byte Developer <145024847+bytesSculptor@users.noreply.github.com>
2023-09-16 07:30:55 +0000

    conversion of data types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+// let score = "33abc";
+// let score = null;
+let score = undefined;
+
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // o/p => NaN - Not a Number (let score = "33abc") // but type is number
+
+// console.log(valueInNumber); // o/p => 0 (let score = null)
+// console.log(valueInNumber); // o/p => NaN (let score = undefined)
+// true => 1; false => 0
+
+
+
+let isLoggedIn = "nameString"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "nameString" => true
+
+
+let someNumber = 33
+// console.log(someNumber);
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
