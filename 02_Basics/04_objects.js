7b2fc017951cfb4b28d1f1d22bd97782701dbce6
Byte Developer <145024847+bytesSculptor@users.noreply.github.com>
2023-09-20 07:41:38 +0530

    Update 04_objects.js

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -25,7 +25,8 @@ const obj4 = {5: "e", 6: "f"}
 
 // const obj3 = {obj1, obj2}
 // const obj3 = Object.assign(obj1, obj2)
-// const obj3 = Object.assign({}, obj1, obj2, obj4) // {}=>target, obj1, obj2 => source
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // ({} =>target, {obj1, obj2, obj4}) => source
+// console.log(obj3);
 
 const obj3 = {...obj1, ...obj2, ...obj4}
 // console.log(obj3);
