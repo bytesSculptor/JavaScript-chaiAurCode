30adc1b14de128b6d04c8f8d96f3f6a26b7cd1ac
Byte Developer <145024847+bytesSculptor@users.noreply.github.com>
2023-09-21 07:27:43 +0530

    Update 05_strings.js

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -16,7 +16,7 @@ const gameName = new String('hitesh-hc-com')
 // console.log(gameName.charAt(2));
 // console.log(gameName.indexOf('t'));
 
-const newString = gameName.substring(0, 4)
+const newString = gameName.substring(0, 4) // hite => o/p
 // console.log(newString);
 
 const anotherString = gameName.slice(-8, 4)
