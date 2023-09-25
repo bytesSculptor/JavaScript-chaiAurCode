0aa766d6be25f85d4bdfdc2bd3774adbb2835d01
Byte Developer <145024847+bytesSculptor@users.noreply.github.com>
2023-09-25 09:37:06 +0000

    project-5 complete

--- 07_projects/5-keyboard/chaiaurcode.js
+++ 07_projects/5-keyboard/chaiaurcode.js
@@ -1,1 +1,29 @@
 console.log('Project 5');
+
+const insert = document.querySelector('#insert');
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+    <div class='color'>
+        <table>
+            <thead>
+                <tr>
+                    <th colspan="3">Press Key and watch</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>Key</th>
+                    <th>Keycode</th> 
+                    <th>Code</th>
+                </tr>
+                <tr>
+                    <td>${e.key === " " ? 'space' : e.key}</td>
+                    <td>${e.keyCode}</td>
+                    <td>${e.code}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `
+})
